56a59ad7c4064d521737c5275a989df707b6f29a
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2025-05-29 17:58:41 -0300

    example

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bora import SegundaSeccion
 
-segunda_seccion = SegundaSeccion(rubros=['CONSTITUCION SA'], fecha_desde="28/05/2025", fecha_hasta="28/05/2025")
+segunda_seccion = SegundaSeccion(rubros=['AVISOS COMERCIALES'], fecha_desde="28/05/2025", fecha_hasta="28/05/2025")
 links = segunda_seccion.get_result()
+print(f"Se han encontrado {len(links)} resultados\n")
 for link in links:
     print(link)
