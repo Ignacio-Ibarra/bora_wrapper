ffcc6a31e01577b8ea1f489fb1936ca89b96c2ce
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2026-04-13 11:24:50 -0300

    add more rubros

--- run_job.sh
+++ run_job.sh
@@ -3,18 +3,12 @@
 export HOME="/home/iibarra"
 export USER="iibarra"
 export PATH="/home/mlrsrv/anaconda3/bin:/usr/bin:/bin"
-# export HTTP_PROXY="http://mlrsrv:OaMv10QQ@10.1.1.96:3128"
-# export HTTPS_PROXY="http://mlrsrv:OaMv10QQ@10.1.1.96:3128"
-# export UV_PYTHON="/home/mlrsrv/anaconda3/bin/python"
-# export UV_PYTHON_DOWNLOADS=never
-# export UV_PYTHON_PREFERENCE=only-managed
 
 # Configuracion
 BASE_DIR="/home/iibarra/work/bora_wrapper"
 LOG_DIR="$BASE_DIR/job_logs"
 LOG_FILE="$LOG_DIR/bora_job_$(date +%Y%m%d).log"
 CURRENT_DATE="$(date +%d/%m/%Y)"
-RUBRO="CONSTITUCION SA"
 
 echo "USER: $(whoami)" >> "$LOG_FILE"
 echo "PATH: $PATH" >> "$LOG_FILE"
@@ -30,7 +24,11 @@ cd "$BASE_DIR" || exit 1
 echo "===== START $(date '+%Y-%m-%d %H:%M:%S') =====" >> "$LOG_FILE"
 
 # Ejecutar job
-/home/iibarra/.local/bin/uv run bora-cli --start-date "$CURRENT_DATE" --end-date "$CURRENT_DATE" --rubro "$RUBRO" >> "$LOG_FILE" 2>&1
+/home/iibarra/.local/bin/uv run bora-cli --start-date "$CURRENT_DATE" --end-date "$CURRENT_DATE" --rubro "CONSTITUCION SA" >> "$LOG_FILE" 2>&1
+
+/home/iibarra/.local/bin/uv run bora-cli --start-date "$CURRENT_DATE" --end-date "$CURRENT_DATE" --rubro "CONTRATO SRL" >> "$LOG_FILE" 2>&1
+
+/home/iibarra/.local/bin/uv run bora-cli --start-date "$CURRENT_DATE" --end-date "$CURRENT_DATE" --rubro "CONSTITUCION SAS" >> "$LOG_FILE" 2>&1
 
 # Guardar codigo de salida
 EXIT_CODE=$?
